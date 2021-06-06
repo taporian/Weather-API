9fe011921f885d0763ad37ba791249f70074518d
RihamBadra <rihamHbadra@gmail.com>
2021-06-06 10:07:46 +0300

    html css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
 
-import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
@@ -10,7 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Riham- Chris- Mahmoud"
+      name: "Riham - Chris - Mahmoud"
     };
   }
 
@@ -21,9 +19,76 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+       {/* ---------Header-------------- */} 
+       
+       <header className="app_header">
+         <nav>
+           <input type="text"  placeholder="Type in a city name"></input>
+           <button type="submit">FIND WEATHER</button>
+         </nav>
+       </header>
+
+       {/* ---------Main-------------- */} 
+
+       <main>
+         
+        <div className="app__main">
+          <img src="http://placekitten.com/g/200/300"></img>
+          <h2>Temperature 12<span>&#176;</span> to 13<span>&#8451;</span></h2>
+          <p><span style={{fontWeight:"bold" , marginRight:"1rem"}}>Humidty</span> 78%  <span style = {{fontWeight:"bold" , margin:"0 1rem"}}>Pressure</span> 1000.8</p>
+        </div>
+
+        {/* -----------Ul------------ */} 
+        <div  >
+          <ul className="wea-ther">
+            
+            <li>
+              <div className="res-grid"><h3>21:00</h3>
+                 <img src="http://placekitten.com/50/50"></img>
+                 <h3>12<span>&#8451;</span></h3></div>
+             </li>
+
+            <li>
+              <div className="res-grid"><h3>21:00</h3>
+                 <img src="http://placekitten.com/50/50"></img>
+                 <h3>12<span>&#8451;</span></h3></div>
+            </li>
+
+            <li>
+              <div className="res-grid"><h3>21:00</h3>
+                 <img src="http://placekitten.com/50/50"></img>
+                 <h3>12<span>&#8451;</span></h3></div>
+            </li>
+
+            <li>
+              <div className="res-grid"><h3>21:00</h3>
+                 <img src="http://placekitten.com/50/50"></img>
+                 <h3>12<span>&#8451;</span></h3></div>
+            </li>
+
+            <li>
+              <div className="res-grid"><h3>21:00</h3>
+                 <img src="http://placekitten.com/50/50"></img>
+                 <h3>12<span>&#8451;</span></h3></div>
+            </li>
+
+            <li>
+              <div className="res-grid"><h3>21:00</h3>
+                 <img src="http://placekitten.com/50/50"></img>
+                 <h3>12<span>&#8451;</span></h3></div>
+            </li>
+
+            <li>
+              <div className="res-grid"><h3>21:00</h3>
+                 <img src="http://placekitten.com/50/50"></img>
+                 <h3>12<span>&#8451;</span></h3></div>
+            </li>
+           
+          
+          </ul>
+        </div>
+       </main>
+        
       </div>
     );
   }
