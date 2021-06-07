fe101e14659aea8fd63b04451e0cafafaa60184f
RihamBadra <rihamHbadra@gmail.com>
2021-06-07 10:58:06 +0300

    Weather API: Extracting the values from the Fake Weather json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+
 import storm from "../src/img/weather-icons/storm.svg"; 
 import clear from "../src/img/weather-icons/clear.svg"; 
 import Search from "../src/components/Search";
 import Weather from "../src/components/WeatherItem"
+import sayHi, {SayHello} from "./components/WeatherItem"
 
-
-import fakeWeatherData from "./fakeWeatherData.json";
+import FakeWeather from  './fakeWeatherData.json';
 
 import "./App.css";
 
@@ -13,7 +14,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Riham - Chris - Mahmoud"
+      name: ""
     };
   }
 
@@ -28,7 +29,9 @@ class App extends Component {
        
        <header className="app_header">
          <nav>
+           
            <Search  handleInput={this.handleInput}/>
+           <SayHello color="black" name={this.state.name} />
 
            <Weather />
            
@@ -42,7 +45,7 @@ class App extends Component {
          
         <div className="app__main">
           <div class="imagee"> <img src={clear}  /> </div>
-          <h2>Temperature 12<span>&#176;</span> to 13<span>&#8451;</span></h2>
+          <h2>Temperature {Math.floor(FakeWeather.list[0].main.temp - 273.15)}<span>&#176;</span> to {Math.floor(FakeWeather.list[1].main.temp - 273.15)}<span>&#8451;</span></h2>
           <p><span style={{fontWeight:"bold" , marginRight:"1rem"}}>Humidity</span> 78%  <span style = {{fontWeight:"bold" , margin:"0 1rem"}}>Pressure</span> 1000.8</p>
         </div>
 
@@ -53,43 +56,43 @@ class App extends Component {
             <li>
               <div className="res-grid"><h3>21:00</h3>
                  <img src={storm} />
-                 <h3>12<span>&#8451;</span></h3></div>
+                 <h3>{Math.floor(FakeWeather.list[2].main.temp - 273.15)}<span>&#8451;</span></h3></div>
              </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
                  <img src={storm} />
-                 <h3>12<span>&#8451;</span></h3></div>
+                 <h3>{Math.floor(FakeWeather.list[3].main.temp - 273.15)}<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
               <img src={storm} /> 
-                 <h3>12<span>&#8451;</span></h3></div>
+                 <h3>{Math.floor(FakeWeather.list[4].main.temp - 273.15)}<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
               <img src={storm} /> 
-                 <h3>12<span>&#8451;</span></h3></div>
+                 <h3>{Math.floor(FakeWeather.list[5].main.temp - 273.15)}<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
               <img src={storm} /> 
-                 <h3>12<span>&#8451;</span></h3></div>
+                 <h3>{Math.floor(FakeWeather.list[5].main.temp - 273.15)}<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
               <img src={storm} /> 
-                 <h3>12<span>&#8451;</span></h3></div>
+                 <h3>{Math.floor(FakeWeather.list[4].main.temp - 273.15)}<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
               <img src={storm} /> 
-                 <h3>12<span>&#8451;</span></h3></div>
+                 <h3>{Math.floor(FakeWeather.list[3].main.temp - 273.15)}<span>&#8451;</span></h3></div>
             </li>
            
           
--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -3,14 +3,16 @@ import { render } from "react-dom";
 
 export default class WeatherItem extends Component {
   render() {
-    return <div>{this.props.name}</div>;
+    return <div> </div>;
   }
 }
+
+
 export class SayHello extends Component {
   render() {
     return (
-      <div style={{ color: this.props.color, backgroundColor: "yellow" }}>
-        Hello {this.props.name}
+      <div style={{ color: this.props.color }}>
+         {this.props.name}
       </div>
     );
   }
