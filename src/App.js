17ebca047d5673354105309fa24b0506cdc9cf08
RihamBadra <rihamHbadra@gmail.com>
2021-06-07 09:17:00 +0300

    clear pics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import storm from "../src/img/weather-icons/storm.svg"; 
+import clear from "../src/img/weather-icons/clear.svg"; 
 
 import fakeWeatherData from "./fakeWeatherData.json";
 
@@ -33,7 +35,7 @@ class App extends Component {
        <main>
          
         <div className="app__main">
-          <img src="../img/weather-icons/storm.svg" alt="storm"></img>
+          <div class="imagee"> <img src={clear}  /> </div>
           <h2>Temperature 12<span>&#176;</span> to 13<span>&#8451;</span></h2>
           <p><span style={{fontWeight:"bold" , marginRight:"1rem"}}>Humidty</span> 78%  <span style = {{fontWeight:"bold" , margin:"0 1rem"}}>Pressure</span> 1000.8</p>
         </div>
@@ -44,43 +46,43 @@ class App extends Component {
             
             <li>
               <div className="res-grid"><h3>21:00</h3>
-                 <img src="h../img/weather-icons/storm.svg"></img>
+                 <img src={storm} />
                  <h3>12<span>&#8451;</span></h3></div>
              </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
-                 <img src="../img/weather-icons/storm.svg"></img>
+                 <img src={storm} />
                  <h3>12<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
-                 <img src="../img/weather-icons/storm.svg"></img>
+              <img src={storm} /> 
                  <h3>12<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
-                 <img src="../img/weather-icons/storm.svg"></img>
+              <img src={storm} /> 
                  <h3>12<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
-                 <img src="../img/weather-icons/storm.svg"></img>
+              <img src={storm} /> 
                  <h3>12<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
-                 <img src="../img/weather-icons/storm.svg"></img>
+              <img src={storm} /> 
                  <h3>12<span>&#8451;</span></h3></div>
             </li>
 
             <li>
               <div className="res-grid"><h3>21:00</h3>
-                 <img src="../img/weather-icons/storm.svg"></img>
+              <img src={storm} /> 
                  <h3>12<span>&#8451;</span></h3></div>
             </li>
            
