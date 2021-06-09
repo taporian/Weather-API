4f4357e1c05bc3b3cf754110bca90feaa8ff9d48
taporian <christapor.har@gmail.com>
2021-06-09 06:23:04 +0300

    Matching up the weather id with the appropriate icon

--- src/App.js
+++ src/App.js
@@ -146,39 +146,5 @@ export default App;
 
 
 
-// weatherIcons= id =>{
-//   if(id<300) {
-//      return storm;
-//   }
-//   else if (id>= 300 && id <= 499){
-//     return drizzle;
-//    }
-     
-//   else if (id>= 500 && id <= 599){
-//     return rain;
-//   }
-//         else if (id>= 600 && id <= 699){
-//           return snow;
-//         }
-
-//     else if (id>= 700 && id <= 799){
-//       return fog;
-//     }
-//     else if(id==800){
-//       return clear;
-//     }
- 
-//     else if(id==801){
-//       return partlycloudy;
-//     }    
-//       if (id>= 801 && id <= 805){
-//         return mostlycloudy;
-//       }
-//     else {
-//       return unknown;
-//     }
-      
-
-// };
 
 
